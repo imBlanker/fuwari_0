6cf7cdfefe4817b64a55b2feb71d2bfbbced4571
imBlanker <jamesibran@hotmail.com>
2025-09-01 02:58:35 +0800

    deployed with wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 // https://astro.build/config
 export default defineConfig({
 	//   site: "https://fuwari.vercel.app/",
-	site: "https://fuwari.6263046.xyz/",
+	site: "https://my-astro-app.ilittleblank.workers.dev/",
 	base: "/",
 	trailingSlash: "always",
 
